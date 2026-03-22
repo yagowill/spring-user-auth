9466cdb9fafc33fe60a67071583403f5719bc136
yagowill <yagowill3@gmail.com>
2026-03-22 18:16:30 -0300

    chore: initial project setup with Spring Boot 3.4.4

---
+++ src/main/java/com/yagowill/springuserauth/SpringUserAuthApplication.java
@@ -0,0 +1,12 @@
+package com.yagowill.springuserauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserAuthApplication.class, args);
+    }
+}
