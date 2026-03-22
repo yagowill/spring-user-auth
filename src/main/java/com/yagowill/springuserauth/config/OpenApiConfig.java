7cdcbe1c7fa3da0410083d571ed7b3740bf17fdf
yagowill <yagowill3@gmail.com>
2026-03-22 18:21:18 -0300

    docs: configure Swagger/OpenAPI documentation

---
+++ src/main/java/com/yagowill/springuserauth/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.yagowill.springuserauth.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring User Auth API")
+                        .description("API REST para gerenciamento de usuários com autenticação JWT")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Yago Will")
+                                .url("https://github.com/yagowill"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Insira o token JWT. Exemplo: eyJhbGciOiJIUzI1NiJ9...")));
+    }
+}
